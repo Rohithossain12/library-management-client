808367e47c89d8f41303be4217e96ca3d2abbde9
Rohithossain12 <https://github.com/Rohithossain12>
2025-07-03 16:17:28 +0600

    basic project setup done

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <Button className='bg-green-500'>Hello World</Button>
+    </>
+  )
+}
+
+export default App
